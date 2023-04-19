9ecbc917885507bc71b9e1b25272fa3873ee46d6
RaeesaParker <raeesaparker101@gmail.com>
2023-04-19 20:20:54 +0100

    Add favicon

--- src/components/skills/techstack.ts
+++ src/components/skills/techstack.ts
@@ -1,1 +1,1 @@
-export const techStackArray = ["HTML", "CSS", "Javascript", "Bootstrap", "React", "Typescript", "Figma", "Node.js", "MongoDB", "SQL", "Git", "Python"]
+export const techStackArray = ["HTML", "CSS", "Javascript", "Bootstrap", "React", "Typescript", "Wordpress", "Figma", "Node.js", "SQL", "Git", "Python"]
