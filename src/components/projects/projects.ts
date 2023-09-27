d62d2a2fc346954461cd8a18b0b31d45e33af62e
Raeesa Parker <31614689+RaeesaParker@users.noreply.github.com>
2023-09-27 15:21:30 +0100

    Update projects.ts - remove savings hub

--- src/components/projects/projects.ts
+++ src/components/projects/projects.ts
@@ -8,14 +8,14 @@ export const projectsArray =
       "demoLink": "https://neighbourwho.netlify.app/",
       "codeLink": "https://github.com/RaeesaParker/neighbourhood-front"
     },
-    {
-      "projectName": "Savings Hub",
-      "projectDescription": "e-Commerce store for South African company Savings Hub. ",
-      "projectImage": "savings",
-      "techStack": ["Wordpress", "WooCommerce"],
-      "demoLink": "https://savingshub.co.za",
-      "codeLink": null
-    },
+    // {
+    //   "projectName": "Savings Hub",
+    //   "projectDescription": "e-Commerce store for South African company Savings Hub. ",
+    //   "projectImage": "savings",
+    //   "techStack": ["Wordpress", "WooCommerce"],
+    //   "demoLink": "https://savingshub.co.za",
+    //   "codeLink": null
+    // },
     {
       "projectName": "PinIt!",
       "projectDescription": "A kanban-style work management tool that can be used to ideate plans and track progress in a visual, productive, and rewarding way.",
